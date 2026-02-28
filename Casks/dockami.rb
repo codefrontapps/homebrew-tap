9d6c02e1d0bb3420c1138745f252dd296169ee56
codefrontapps <codefrontapps@users.noreply.github.com>
2026-02-28 19:52:09 +0100

    Switch to ZIP format

--- Casks/dockami.rb
+++ Casks/dockami.rb
@@ -1,8 +1,8 @@
 cask "dockami" do
   version "1.2.1"
-  sha256 "3cfd956a77c17fcef1d7bcd3e2e37f102a7ad4e7434b86bc221850702b9cd0cf"
+  sha256 "8bcbae1fbb9a226f82c6e922a8f10746525e2d72fca0bbaa8fc587fe0e976407"
 
-  url "https://github.com/codefrontapps/DockAmi/releases/download/v#{version}/DockAmi.dmg"
+  url "https://github.com/codefrontapps/DockAmi/releases/download/v#{version}/DockAmi.zip"
   name "DockAmi"
   desc "Customize your macOS Dock with profiles"
   homepage "https://dockami.app"
