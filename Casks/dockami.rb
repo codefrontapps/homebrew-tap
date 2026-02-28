a18a2093edbf6358b05232ecd0fce3f4c109c682
codefrontapps <codefrontapps@users.noreply.github.com>
2026-02-28 19:06:11 +0100

    Update DockAmi to v1.2.0

--- Casks/dockami.rb
+++ Casks/dockami.rb
@@ -1,6 +1,6 @@
 cask "dockami" do
-  version "1.1.0"
-  sha256 "c04818af8a19135a8295b52c3603606faea924d190149a4835d1280a19aa02ca"
+  version "1.2.0"
+  sha256 "48a79e20ac1a73c638bb2f03663d99e8fa23dc5dd47e27939bdfaddd4ac454f9"
 
   url "https://github.com/codefrontapps/DockAmi/releases/download/v#{version}/DockAmi.dmg"
   name "DockAmi"
